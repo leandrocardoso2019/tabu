1a3665518da2d4b1ad4eba0666f09ac1ded1bf2a
Leandro Cardoso <Leandropantas@outlook.com>
2019-07-22 08:55:07 -0300

    Estrutura

--- Program.cs
+++ Program.cs
@@ -6,7 +6,14 @@ namespace Tabuada
     {
         static void Main(string[] args)
         {
-            
+            int numero;
+            //limpar tela de console
+            Console.Clear();
+            Console.WriteLine("#####################################");
+            Console.WriteLine("#        Programa de tabuada         ");
+            Console.WriteLine("#####################################");
+            Console.WriteLine();
+            Console.WriteLine("Digite um numero >");
         }
     }
 }
